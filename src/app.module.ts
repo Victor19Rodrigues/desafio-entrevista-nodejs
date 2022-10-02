008b2a785efd9bcbb0d868ee74d5dcc2b9d68dbb
Victor Rodrigues <vitor1908@gmail.com.br>
2022-10-02 20:40:04 -0300

    feat: add typeorm lib and configurate

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CompaniesModule } from './companies/companies.module';
 import { VehiclesModule } from './vehicles/vehicles.module';
 
 @Module({
-  imports: [CompaniesModule, VehiclesModule],
+  imports: [
+    CompaniesModule,
+    VehiclesModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'park_db',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
