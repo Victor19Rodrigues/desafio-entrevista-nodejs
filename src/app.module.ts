feac4d3ca1852e5762dae6425e354dde8c028c5f
Victor Rodrigues <vitor1908@gmail.com.br>
2022-10-02 18:39:44 -0300

    feat: create structure for vehicles

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { CompaniesModule } from './companies/companies.module';
+import { VehiclesModule } from './vehicles/vehicles.module';
 
 @Module({
-  imports: [CompaniesModule],
+  imports: [CompaniesModule, VehiclesModule],
 })
 export class AppModule {}
