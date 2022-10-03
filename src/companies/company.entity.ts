3f2355744c151b1deb65d86ea2961f55e282ac25
Victor Rodrigues <vitor1908@gmail.com.br>
2022-10-02 21:00:01 -0300

    feat: create company entity

---
+++ src/companies/company.entity.ts
@@ -0,0 +1,37 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class CompanyEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column({ unique: true })
+  cnpj: string;
+
+  @Column()
+  address: string;
+
+  @Column()
+  phone: string;
+
+  @Column({ name: 'qtd_spots_for_motorcycles' })
+  qtdSpotsForMotorcycles: number;
+
+  @Column({ name: 'qtd_spots_for_cars' })
+  qtdSpotsForCars: number;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+}
