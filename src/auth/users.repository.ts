2bdab26a7c2e90960b3d588f65276ffdd69ce045
Victor Rodrigues <vitor1908@gmail.com.br>
2022-10-03 08:17:32 -0300

    feat: add hash to password

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -4,6 +4,7 @@ import {
   InternalServerErrorException,
 } from '@nestjs/common';
 import { DataSource, Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
 
@@ -18,7 +19,11 @@ export class UsersRepository extends Repository<User> {
   async createUser(authCredentialsData: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsData;
 
-    const user = this.create({ username, password });
+    const salt = await bcrypt.genSalt();
+
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.create({ username, password: hashedPassword });
 
     try {
       await this.save(user);
